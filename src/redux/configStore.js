7ead00a09f1d59af1172c75e45e8f5e23b7f4cec
rsundar <rohan.s.1987@gmail.com>
2021-09-27 20:53:46 +0530

    Removed logging from the reducer middleware

--- src/redux/configStore.js
+++ src/redux/configStore.js
@@ -1,11 +1,10 @@
 import { createStore, applyMiddleware } from 'redux';
-import logger from 'redux-logger';
 import thunkMiddleware from 'redux-thunk';
 import covidDataReducer from './data/covidData';
 
 const store = createStore(
   covidDataReducer,
-  applyMiddleware(thunkMiddleware, logger),
+  applyMiddleware(thunkMiddleware),
 );
 
 export default store;
