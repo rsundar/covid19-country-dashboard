13c797e838f94de60fba1cba63c156e27cb438d4
rsundar <rohan.s.1987@gmail.com>
2021-09-27 20:51:56 +0530

    Added required functional icons on the nav bar

--- src/App.js
+++ src/App.js
@@ -20,6 +20,10 @@ function App() {
             HOME
           </Link>
           <h3 className="m-0">COVID-19 CASELOAD</h3>
+          <span>
+            <i className="me-4 fas fa-microphone" />
+            <i className="fas fa-cog" />
+          </span>
         </Navbar>
       </header>
       <main>
